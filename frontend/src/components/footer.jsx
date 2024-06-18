c7c951439c8164de587911d550db819107e44a01
iovann <iovannatcho@gmail.com>
2024-06-18 12:28:12 +0100

    first day two

---
+++ frontend/src/components/footer.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            <footer className="text-center text-lg-start bg-body-tertiary text-muted py-4">
+                <section className="">
+                    <div className="container text-center text-md-start mt-5">
+                        <div className="row mt-3">
+                            <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                                <h6 className="text-uppercase fw-bold mb-4">Company name</h6>
+                                <p>HBienvenue sur notre plateforme de partage de recettes culinaires, où les passionnés de cuisine se rencontrent et s'inspirent mutuellement. Découvrez une multitude de recettes savoureuses, partagez vos propres créations, et faites partie d'une communauté culinaire dynamique. Ensemble, rendons chaque repas spécial.</p>
+                            </div>
+
+                            <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+
+                                <h6 className="text-uppercase fw-bold mb-4">
+                                    Products
+                                </h6>
+                                <p>
+                                    <a href="#!" className="text-reset">Angular</a>
+                                </p>
+                                <p>
+                                    <a href="#!" className="text-reset">React</a>
+                                </p>
+                                <p>
+                                    <a href="#!" className="text-reset">Vue</a>
+                                </p>
+                                <p>
+                                    <a href="#!" className="text-reset">Laravel</a>
+                                </p>
+                            </div>
+
+                            <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+
+                                <h6 className="text-uppercase fw-bold mb-4">Useful links</h6>
+                                <p>
+                                    <a href="#!" className="text-reset">Pricing</a>
+                                </p>
+                                <p>
+                                    <a href="#!" className="text-reset">Settings</a>
+                                </p>
+                                <p>
+                                    <a href="#!" className="text-reset">Orders</a>
+                                </p>
+                                <p>
+                                    <a href="#!" className="text-reset">Help</a>
+                                </p>
+                            </div>
+                            <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+
+                                <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
+                                <p><i className="fas fa-home me-3"></i> New York, NY 10012, US</p>
+                                <p>
+                                    <i className="fas fa-envelope me-3"></i>
+                                    info@example.com
+                                </p>
+                                <p><i className="fas fa-phone me-3"></i> + 01 234 567 88</p>
+                                <p><i className="fas fa-print me-3"></i> + 01 234 567 89</p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <div className="text-center p-4">
+                    © 2021 Copyright:
+                    <a className="text-reset fw-bold" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+                </div>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
